b999458a45d54b666c169ec2f609c129a5977a2f
Lee Massey <lee.m.massey@gmail.com>
2020-11-06 11:56:55 -0600

    add click functionality to mark the board

--- js/main.js
+++ js/main.js
@@ -1,19 +1,40 @@
 const Gameboard = (() => {
 	const board = [
-		'x','x','x',
-		'x','x','x',
-		'x','x','x'
+		'','','',
+		'','','',
+		'','',''
 	];
 
 	return {board};
 })();
 
-function renderBoardContents() {
-	const gameCells = document.querySelectorAll('.game_cell');
+const Player = (name, mark) => {
+	return { name, mark };
+}
 
+const player1 = Player("Player1", "X");
+const player2 = Player("Player2", "O");
+let currentPlayer = player1;
+
+const gameCells = document.querySelectorAll('.game_cell');
+
+for(let i=0; i<gameCells.length; i++) {
+	gameCells[i].addEventListener("click", function() {
+		if(Gameboard.board[i] === '') {
+			Gameboard.board[i] = currentPlayer['mark'];
+			if(currentPlayer === player1) {
+				currentPlayer = player2;
+			}
+			else {
+				currentPlayer = player1;
+			}
+			renderBoardContents();
+		}
+	});
+}
+
+function renderBoardContents() {
 	for(let i=0; i<Gameboard.board.length; i++) {
 		gameCells[i].textContent = Gameboard.board[i];
 	}
 }
-
-renderBoardContents();
